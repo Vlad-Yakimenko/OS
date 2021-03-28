5e875302b4499645b23707ca3f5d7b733cc73f06
aoyako <ml.logachevsky@gmail.com>
2021-03-28 20:19:49 +0300

    Add Disk class and interface

---
+++ second-semester/Filesystem/src/main/java/ua/knu/io/disk/Disk.java
@@ -0,0 +1,11 @@
+package ua.knu.io.disk;
+
+// Disk interface
+public interface Disk {
+
+    // ReadBlock read block with id blockID
+    public byte[] ReadBlock(int blockID);
+
+    // WriteBlock write block with id blockID
+    public void WriteBlock(byte[] block, int blockID);
+}

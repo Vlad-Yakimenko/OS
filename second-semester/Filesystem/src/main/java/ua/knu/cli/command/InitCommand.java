71c324dfbbeb080e2dbf443396424978bb945441
Vlad Yakymenko <vladyslav.yakymenko@intapp.com>
2021-04-28 16:22:18 +0300

    FS-4: Init command implemented

---
+++ second-semester/Filesystem/src/main/java/ua/knu/cli/command/InitCommand.java
@@ -0,0 +1,39 @@
+package ua.knu.cli.command;
+
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.val;
+import ua.knu.cli.view.View;
+import ua.knu.io.disk.DiskInitializer;
+import ua.knu.util.Constants;
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true)
+public class InitCommand extends Command {
+
+    private View view;
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.equals("in");
+    }
+
+    @Override
+    public void process(String command) {
+        val parameters = command.split(Constants.COMMAND_SEPARATOR);
+
+        verifyCorrectParametersAmount(parameters.length);
+
+        if (!DiskInitializer.isInitialized) {
+            DiskInitializer.initialize();
+            view.write("disk initialized");
+        } else {
+            view.write("disk restored");
+        }
+    }
+
+    @Override
+    public String getCommandSample() {
+        return "in";
+    }
+}
